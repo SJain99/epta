6b24d0ab667b7a3c64d59aa7d82177e4b65e9884
Saransh Jain <saransh.jain@mail.utoronto.ca>
2021-11-29 19:55:30 -0500

    HTTPS fix for PDF file viewer.

--- components/TechnicalPDFCard.js
+++ components/TechnicalPDFCard.js
@@ -12,7 +12,7 @@ const TechnicalPDFCard = ({profile}) => {
                 width="100%"
             >
             <IconButton icon={<ArrowBackIcon />} fontSize="18px" marginRight="8px" variant="solid" borderRadius="full" onClick={() => index == 0 ? setIndex(profile.fields.technicalProjects.length-1) : setIndex(index - 1)} />
-            <iframe src={"http://docs.google.com/viewer?url=" + "https:" + profile.fields.technicalProjects[index].fields.file.url + "&embedded=true"} style={{width: "100%", height: "70vh", marginBottom: 16}} frameBorder="0"></iframe>
+            <iframe src={"https://docs.google.com/viewer?url=" + "https:" + profile.fields.technicalProjects[index].fields.file.url + "&embedded=true"} style={{width: "100%", height: "70vh", marginBottom: 16}} frameBorder="0"></iframe>
             <IconButton icon={<ArrowForwardIcon />} fontSize="18px" marginLeft="8px" variant="solid" borderRadius="full" onClick={() => index == profile.fields.technicalProjects.length-1 ? setIndex(0) : setIndex(index + 1)} />
             </Flex>
             <Text fontSize="2xl" fontWeight="bold" px={12}>{profile.fields.technicalProjects[index].fields.title}</Text>
