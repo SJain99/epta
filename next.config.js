492ff2704f5c54736b60c586c85a08790d5f51fe
Saransh Jain <saransh.jain@mail.utoronto.ca>
2021-11-29 20:44:48 -0500

    Some aesthetic changes.

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
-const securityHeaders = [{
-  key: 'Content-Security-Policy-Report-Only',
-  value: ''
-}]
-
 module.exports = {
   reactStrictMode: true,
   images: {
     domains: ["images.ctfassets.net", "assets.ctfassets.net"]
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: securityHeaders,
-      }
-    ]
   }
 }
